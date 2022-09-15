cc5f7868ba0f4b6c9f5a153531e92646854e18ad
RebeccaRamalho <kanurebecca0@gmail.com>
2022-09-15 19:03:00 +0200

    test: test of data type for the register done

--- HOH_Back/src/tests/units/userService.test.ts
+++ HOH_Back/src/tests/units/userService.test.ts
@@ -4,97 +4,224 @@ import { mailerService } from "../../libs";
 
 // import { User } from "../../modules/User/entity";
 // import { IUserRepository } from "../../helpers/interfaces/article.interfaces";
-let emailTestValues = [
-    "",
-    "k",
-    "k@gmail.com",
-    "kanu_@gmail.com",
-    "kanu__@gmail.com",
-    "_kanu@gmail.com",
-    "kanurebecca@_gmail.com",
-    "kanurebecca@gmail_.com",
-    "kanurebecca@gmail.c",
-    "kanurebecca@gmail.c_",
-    "kanu.-rebecca..@gmail.c_",
-];
-let passwordTestValues = [
-  "",
-  "k",
-  "kanuAmelia",
-  "KANUEMMANUELLE1899",
-  "kanuemmanuelle1899",
-  "1989189918911",
-  "KANUEMMANUELLE1899.",
-  "kanuemmanuelle1899.",
-  "1989189918911.",
-];
 
 const userService = new UserService(new UserRepositoryMock(), mailerService);
 
 describe("Admin service USE-CASE: ", () => {
   describe("add admin use case:", () => {
-    test("register function exist", () => {
+    //SUCCESS
+    it("Should thrown an error if the register function doesn't exist", async () => {
       expect(userService.register).toBeDefined();
     });
 
-    // test.each([[1, 1, 2], [1, 2, 3], [2, 1, 3]])(
-    //     '.add(%i, %i)',
-    //     (a, b, expected) => {
-    //       expect(a + b).toBe(expected);
-    //     },
-    //   );
-
-    //répéter le test autant de fois qu'il y'a de valeur dans la variable emailTestValues
-
-    it("Should throw an error if if the email is not in the good format", async () => {
+    //ERROR
+    it("Should throw an error if the email is empty", async () => {
       try {
-        for (let index = 0; index >= emailTestValues.length; index++) {
-          await userService.register({
-            user_name: "Amélia",
-            email: emailTestValues[index],
-            password: "rebecca2009GOOD",
-          });
-        }
+        await userService.register({
+          user_name: "Amélia",
+          email: "",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the email is too short", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "k@gmail.com",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the email have two consecutive special characters", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanu__@gmail.com",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the @ of the mail is preceded by a special character", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanu_@gmail.com",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the mail begin with a special character", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "_kanu@gmail.com",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the @ of the mail is followed by a special character", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanurebecca@_gmail.com",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the . of the mail is preceded by a special character", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanurebecca@gmail_.com",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the domain name of the mail too short", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanurebecca@gmail.c",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    it("Should throw an error if the domain name of the mail is followed by a special character", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanurebecca@gmail.com_",
+          password: "Rebecca2009.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe("Le mail n'est pas au bon format.");
+      }
+    });
+    //
+    it("Should throw an error if if the password is empty", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanuAmélia@gmail.com",
+          password: "",
+        });
       } catch (e: any) {
         expect(e.statusCode).toBe(400);
         expect(e.message).toBe(
-          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale" ||
-            "Le mail n'est pas au bon format."
+          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale"
         );
       }
     });
-
-    // it("Should throw an error if if the email is not in the good format", async () => {
-    //   try {
-    //     const user = await userService.register({
-    //       user_name: "Amélia",
-    //       email: emailTestValues,
-    //       password: "rebecca2009GOOD",
-    //     });
-    //   } catch (e: any) {
-    //     expect(e.statusCode).toBe(400);
-    //     expect(e.message).toBe(
-    //       "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale" ||
-    //         "Le mail n'est pas au bon format."
-    //     );
-    //   }
-    // });
-
-    // it("Should throw an error if the password is not in the good format", async () => {
-    //   try {
-    //     await userService.register({
-    //       user_name: "Amélia",
-    //       email: "kanuAmélia@gmail.com",
-    //       password: passwordTestValues,
-    //     });
-    //   } catch (e: any) {
-    //     expect(e.statusCode).toBe(400);
-    //     expect(e.message).toBe(
-    //       "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale" ||
-    //         "Le mail n'est pas au bon format."
-    //     );
-    //   }
-    // });
+    it("Should throw an error if if the password is too short", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanuAmélia@gmail.com",
+          password: "kR1.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe(
+          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale"
+        );
+      }
+    });
+    it("Should throw an error if the password have not lower case", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanuAmélia@gmail.com",
+          password: "KANUEMMANUELLE1899.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe(
+          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale"
+        );
+      }
+    });
+    it("Should throw an error if the password have not upper case", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanuAmélia@gmail.com",
+          password: "kanuemmanuelle1899.",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe(
+          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale"
+        );
+      }
+    });
+    it("Should throw an error if the password have no numbers", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanuAmélia@gmail.com",
+          password: "KanuAmélia_",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe(
+          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale"
+        );
+      }
+    });
+    it("Should throw an error if the password have ony numbers", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanuAmélia@gmail.com",
+          password: "20020020022002",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe(
+          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale"
+        );
+      }
+    });
+    it("Should throw an error if the password have no special characters", async () => {
+      try {
+        await userService.register({
+          user_name: "Amélia",
+          email: "kanuAmélia@gmail.com",
+          password: "KanuAmélia1",
+        });
+      } catch (e: any) {
+        expect(e.statusCode).toBe(400);
+        expect(e.message).toBe(
+          "Le mot de Passe n'est pas au bon format. Il doit contenir au moins 8 caractères composés à minima d'une lettre minuscule, majuscule, d'un chiffre et d'un caractère spéciale"
+        );
+      }
+    });
     //   expect(typeof user.password).toBe("string");
   });
 });
